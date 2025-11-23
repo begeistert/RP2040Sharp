bc677429e0721d0096b2b29905358299df1965b7
Ivan Montiel <ivan_montiel2013@hotmail.com>
2025-11-23 00:15:09 -0600

    feat: Remove AddsImmediate8 and empty MovsImmediate in BitOps

--- src/RP2040.Core/Cpu/Instructions/BitOps.cs
+++ src/RP2040.Core/Cpu/Instructions/BitOps.cs
@@ -6,29 +6,8 @@ namespace RP2040.Core.Cpu.Instructions;
 public static class BitOps
 {
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static void MovsImmediate(ushort opcode, CortexM0Plus cpu)
+	public static void Ands(ushort opcode, CortexM0Plus cpu)
 	{
-		// MOVS Rd, #imm8
-		// Encoding: 0010 0ddd iiii iiii
-		var rd = (int)BitUtils.Extract(opcode, 8, 3);
-		var imm8 = BitUtils.Extract(opcode, 0, 8);
-
-		cpu.Registers[rd] = imm8;
-		cpu.SetZN(imm8); // Método público/interno de CortexM0Plus
-	}
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static void AddsImmediate8(ushort opcode, CortexM0Plus cpu)
-	{
-		// ADDS Rd, #imm8 (Rd también es Rn)
-		// Encoding: 0011 0ddd iiii iiii
-		int rd = (int)BitUtils.Extract(opcode, 8, 3);
-		uint imm8 = BitUtils.Extract(opcode, 0, 8);
-
-		uint op1 = cpu.Registers[rd];
-		uint result = op1 + imm8;
-
-		cpu.Registers[rd] = result;
-		cpu.SetFlagsAdd(op1, imm8, result);
+		
 	}
 }
