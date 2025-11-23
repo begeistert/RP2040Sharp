e355b4652e0ebb53637ad694ebdbb75e578b2743
Ivan Montiel <ivan_montiel2013@hotmail.com>
2025-11-22 21:43:41 -0600

    feat: Introduce IMemoryMappedDevice interface

---
+++ src/RP2040.Core/Memory/IMemoryMappedDevice.cs
@@ -0,0 +1,14 @@
+namespace RP2040.Core.Memory;
+
+public interface IMemoryMappedDevice
+{
+	uint Size { get; }
+    
+	byte ReadByte(uint offset);
+	ushort ReadHalfWord(uint offset);
+	uint ReadWord(uint offset);
+
+	void WriteByte(uint offset, byte value);
+	void WriteHalfWord(uint offset, ushort value);
+	void WriteWord(uint offset, uint value);
+}
