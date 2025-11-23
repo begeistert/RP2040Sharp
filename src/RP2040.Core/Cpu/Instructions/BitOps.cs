8d6de74b5c561da098971c4abcd70035f40e80fc
Ivan Montiel <ivan_montiel2013@hotmail.com>
2025-11-23 02:12:40 -0600

    Feat: Implement ASRS (Arithmetic Shift Right Signed) instruction

--- src/RP2040.Core/Cpu/Instructions/BitOps.cs
+++ src/RP2040.Core/Cpu/Instructions/BitOps.cs
@@ -19,4 +19,35 @@ public static class BitOps
 		cpu.Registers.N = (int)result < 0; 
 		cpu.Registers.Z = (result == 0);
 	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void AsrsImm5(ushort opcode, CortexM0Plus cpu)
+	{
+		var rd = opcode & 0x7;
+		var rm = (opcode >> 3) & 0x7;
+		var imm5 = (opcode >> 6) & 0x1F;
+
+		ref var ptrRd = ref cpu.Registers[rd];
+		var valRm = cpu.Registers[rm];
+
+		uint result;
+		bool carry;
+
+		if (imm5 == 0)
+		{
+			result = (uint)((int)valRm >> 31);
+			carry = (int)valRm < 0;
+		}
+		else
+		{
+			result = (uint)((int)valRm >> imm5);
+			carry = ((valRm >> (imm5 - 1)) & 1) != 0;
+		}
+
+		ptrRd = result;
+
+		cpu.Registers.N = (int)result < 0; // Bit 31
+		cpu.Registers.Z = (result == 0);
+		cpu.Registers.C = carry;
+	}
 }
