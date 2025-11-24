f749e8b4f23ec8eb963b20e04ef377a560f6c391
Ivan Montiel <ivan_montiel2013@hotmail.com>
2025-11-23 20:34:26 -0600

    feat(cpu): Introduce placeholder handler for barrier instructions

--- src/RP2040.Core/Cpu/Instructions/SystemOps.cs
+++ src/RP2040.Core/Cpu/Instructions/SystemOps.cs
@@ -1,6 +1,12 @@
+using System.Runtime.CompilerServices;
 namespace RP2040.Core.Cpu.Instructions;
 
 public class SystemOps
 {
-	
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void Barrier(ushort opcodeH1, CortexM0Plus cpu)
+	{
+		cpu.Registers.PC += 2;
+		cpu.Cycles += 2;
+	}
 }
