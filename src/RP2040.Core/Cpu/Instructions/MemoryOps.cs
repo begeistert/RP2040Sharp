7790a6f9ef6b4ef7a31d7bec91e1805934bca881
Ivan Montiel <ivan_montiel2013@hotmail.com>
2025-11-26 18:32:59 -0600

    feat: Implement the `POP` instruction (T1 encoding)

--- src/RP2040.Core/Cpu/Instructions/MemoryOps.cs
+++ src/RP2040.Core/Cpu/Instructions/MemoryOps.cs
@@ -1,6 +1,58 @@
+using System.Numerics; // Vital para BitOperations
+using System.Runtime.CompilerServices;
+using RP2040.Core.Memory;
+
 namespace RP2040.Core.Cpu.Instructions;
 
-public class MemoryOps
+public unsafe class MemoryOps
 {
-	
+	[MethodImpl (MethodImplOptions.AggressiveInlining)]
+	public static void Pop (ushort opcode, CortexM0Plus cpu)
+	{
+		var mask = (uint)(opcode & 0xFF);
+		var hasPc = (opcode & 0x100) != 0; // Bit 8 (P-bit)
+
+		var sp = cpu.Registers.SP;
+
+		if (sp >> 28 == BusInterconnect.REGION_SRAM) {
+			var ptr = cpu.Bus.PtrSram;
+			const uint memMask = BusInterconnect.MASK_SRAM;
+			
+			while (mask != 0) {
+				var regIdx = BitOperations.TrailingZeroCount (mask);
+				
+				cpu.Registers[regIdx] = Unsafe.ReadUnaligned<uint> (ptr + (sp & memMask));
+
+				sp += 4;
+				cpu.Cycles++;
+				mask &= (mask - 1);
+			}
+
+			if (hasPc) {
+				var newPc = Unsafe.ReadUnaligned<uint> (ptr + (sp & memMask));
+				sp += 4;
+
+				cpu.Registers.PC = newPc & 0xFFFFFFFE;
+				cpu.Cycles += 2;
+			}
+		}
+		else {
+			while (mask != 0) {
+				var regIdx = BitOperations.TrailingZeroCount (mask);
+				cpu.Registers[regIdx] = cpu.Bus.ReadWord (sp);
+				sp += 4;
+				cpu.Cycles++;
+				mask &= (mask - 1);
+			}
+
+			if (hasPc) {
+				var newPc = cpu.Bus.ReadWord (sp);
+				sp += 4;
+				cpu.Registers.PC = newPc & 0xFFFFFFFE;
+				cpu.Cycles += 2;
+			}
+		}
+		
+		cpu.Registers.SP = sp;
+	}
 }
