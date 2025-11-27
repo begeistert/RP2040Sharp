0d7922162b1aee992cdbaa90b9062142c0dba28f
Ivan Montiel <ivan_montiel2013@hotmail.com>
2025-11-27 02:46:53 -0600

    ARM: Refactor MemoryOps, split Pop instruction into Pop and PopPc

--- src/RP2040.Core/Cpu/Instructions/MemoryOps.cs
+++ src/RP2040.Core/Cpu/Instructions/MemoryOps.cs
@@ -1,39 +1,27 @@
-using System.Numerics; // Vital para BitOperations
+using System.Numerics;
 using System.Runtime.CompilerServices;
 using RP2040.Core.Memory;
 
 namespace RP2040.Core.Cpu.Instructions;
 
-public unsafe class MemoryOps
+public unsafe static class MemoryOps
 {
 	[MethodImpl (MethodImplOptions.AggressiveInlining)]
 	public static void Pop (ushort opcode, CortexM0Plus cpu)
 	{
 		var mask = (uint)(opcode & 0xFF);
-		var hasPc = (opcode & 0x100) != 0; // Bit 8 (P-bit)
-
-		var sp = cpu.Registers.SP;
+		var sp = cpu.Registers.SP; 
 
 		if (sp >> 28 == BusInterconnect.REGION_SRAM) {
 			var ptr = cpu.Bus.PtrSram;
 			const uint memMask = BusInterconnect.MASK_SRAM;
-			
+
 			while (mask != 0) {
 				var regIdx = BitOperations.TrailingZeroCount (mask);
-				
 				cpu.Registers[regIdx] = Unsafe.ReadUnaligned<uint> (ptr + (sp & memMask));
-
 				sp += 4;
 				cpu.Cycles++;
 				mask &= (mask - 1);
-			}
-
-			if (hasPc) {
-				var newPc = Unsafe.ReadUnaligned<uint> (ptr + (sp & memMask));
-				sp += 4;
-
-				cpu.Registers.PC = newPc & 0xFFFFFFFE;
-				cpu.Cycles += 2;
 			}
 		}
 		else {
@@ -44,15 +32,47 @@ public unsafe class MemoryOps
 				cpu.Cycles++;
 				mask &= (mask - 1);
 			}
+		}
+		cpu.Registers.SP = sp;
+	}
 
-			if (hasPc) {
-				var newPc = cpu.Bus.ReadWord (sp);
+	[MethodImpl (MethodImplOptions.AggressiveInlining)]
+	public static void PopPc (ushort opcode, CortexM0Plus cpu)
+	{
+		var mask = (uint)(opcode & 0xFF);
+		var sp = cpu.Registers.SP;
+		
+		if (sp >> 28 == BusInterconnect.REGION_SRAM) {
+			var ptr = cpu.Bus.PtrSram;
+			const uint memMask = BusInterconnect.MASK_SRAM;
+
+			while (mask != 0) {
+				var regIdx = BitOperations.TrailingZeroCount (mask);
+				cpu.Registers[regIdx] = Unsafe.ReadUnaligned<uint> (ptr + (sp & memMask));
 				sp += 4;
-				cpu.Registers.PC = newPc & 0xFFFFFFFE;
-				cpu.Cycles += 2;
+				cpu.Cycles++;
+				mask &= (mask - 1);
 			}
+
+			var newPc = Unsafe.ReadUnaligned<uint> (ptr + (sp & memMask));
+			sp += 4;
+			cpu.Registers.PC = newPc & 0xFFFFFFFE;
+		}
+		else {
+			while (mask != 0) {
+				var regIdx = BitOperations.TrailingZeroCount (mask);
+				cpu.Registers[regIdx] = cpu.Bus.ReadWord (sp);
+				sp += 4;
+				cpu.Cycles++;
+				mask &= (mask - 1);
+			}
+
+			var newPc = cpu.Bus.ReadWord (sp);
+			sp += 4;
+			cpu.Registers.PC = newPc & 0xFFFFFFFE;
 		}
 		
+		cpu.Cycles += 2;
 		cpu.Registers.SP = sp;
 	}
 }
