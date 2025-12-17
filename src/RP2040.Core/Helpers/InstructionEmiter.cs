2d6f980f849ee5c98ab4877392f600465a500c3e
Ivan Montiel <ivan_montiel2013@hotmail.com>
2025-12-16 20:06:20 -0600

    feat: Introduce and use register range error message constants

--- src/RP2040.Core/Helpers/InstructionEmiter.cs
+++ src/RP2040.Core/Helpers/InstructionEmiter.cs
@@ -2,12 +2,15 @@ namespace RP2040.Core.Helpers;
 
 public static class InstructionEmiter
 {
+	const string LowRegisterIndexOutOfRange = "Register index out of range (0-7)";
+	const string HighRegisterIndexOutOfRange = "Register index out of range (0-15)";
+	
 	// ADCS Rd, Rm
 	// Encoding: 0100 0001 01mm mddd (0x4140 base)
 	public static ushort Adcs (int rd, int rm)
 	{
 		// Validaciones de "Eminencia": Fail fast si el test está mal escrito
-		if (rd > 7 || rm > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rd > 7 || rm > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 
 		return (ushort)(0x4140 | (rm << 3) | rd);
 	}
@@ -23,7 +26,7 @@ public static class InstructionEmiter
 	{
 		imm8 >>= 2;
 		if (imm8 > 0xFF) throw new ArgumentException ("Immediate too large for ADD SP, immediate");
-		if (rd > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rd > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		return (ushort)((uint)(0xA800 | rd << 8) | imm8);
 	}
 
@@ -35,53 +38,53 @@ public static class InstructionEmiter
 
 	public static ushort AddsImm3 (uint rd, uint rn, uint imm3)
 	{
-		if (rd > 7 || rn > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rd > 7 || rn > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		if (imm3 > 7) throw new ArgumentException ("Immediate too large for ADDS");
 		return (ushort)(0x1C00 | (imm3 & 0x7) << 6 | ((rn & 0x07) << 3) | (rd & 0x07));
 	}
 
 	public static ushort AddsImm8 (uint rdn, uint imm8)
 	{
-		if (rdn > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rdn > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		if (imm8 > 255) throw new ArgumentException ("Immediate too large for ADDS");
 		return (ushort)(0x3000 | (rdn & 0x07) << 8 | (imm8 & 0xFF));
 	}
 
 	public static ushort AddsRegister (uint rd, uint rn, uint rm)
 	{
-		if (rd > 7 || rn > 7 || rm > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rd > 7 || rn > 7 || rm > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		return (ushort)(0x1800 | ((rm & 0x07) << 6) | (rn & 0x07) << 3 | (rd & 0x07));
 	}
 
 	public static ushort Adr (uint rd, uint imm8)
 	{
-		if (rd > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rd > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		if (imm8 >> 2 > 0xFF) throw new ArgumentException ("Immediate too large");
 		return (ushort)(0xA000 | (rd & 7) << 8 | imm8 >> 2 & 0xFF);
 	}
 
 	public static ushort Ands (uint rn, uint rm)
 	{
-		if (rn > 7 || rm > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rn > 7 || rm > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		return (ushort)(0x4000 | (rm & 7) << 3 | rn & 7);
 	}
 
 	public static ushort AsrsImm5 (uint rd, uint rm, uint imm5)
 	{
-		if (rd > 7 || rm > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rd > 7 || rm > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		if (imm5 > 31) throw new ArgumentException ("Immediate too large for ASRS");
 		return (ushort)(0x1000 | (imm5 & 0x1F) << 6 | ((rm & 7) << 3) | (rd & 7));
 	}
 
 	public static ushort AsrsRegister (uint rdn, uint rm)
 	{
-		if (rdn > 7 || rm > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rdn > 7 || rm > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		return (ushort)(0x4100 | ((rm & 7) << 3) | rdn);
 	}
 
 	public static ushort Bics (uint rdn, uint rm)
 	{
-		if (rdn > 7 || rm > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rdn > 7 || rm > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		return (ushort)(0x4380 | ((rm & 7) << 3) | rdn);
 	}
 
@@ -103,7 +106,7 @@ public static class InstructionEmiter
 
 	public static ushort Blx (int rm)
 	{
-		if (rm > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rm > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		return (ushort)(0x4780 | rm << 3);
 	}
 
@@ -124,19 +127,19 @@ public static class InstructionEmiter
 
 	public static ushort Bx (uint rm)
 	{
-		if (rm > 15) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rm > 15) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		return (ushort)(0x4700 | rm << 3);
 	}
 
 	public static ushort Cmn (uint rn, uint rm)
 	{
-		if (rn > 7 || rm > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rn > 7 || rm > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		return (ushort)(0x42c0 | ((rn & 7) << 3) | rm);
 	}
 
 	public static ushort CmpImm (uint rn, uint imm8)
 	{
-		if (rn > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rn > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		if (imm8 > 255) throw new ArgumentException ("Immediate too large for CMP");
 		return (ushort)(0x2800 | (rn & 7) << 3 | imm8 & 0xFF);
 	}
@@ -149,65 +152,53 @@ public static class InstructionEmiter
 
 	public static ushort CmpHighRegister (uint rn, uint rm)
 	{
-		if (rn > 15 || rm > 15) throw new ArgumentException ("Register index out of range (0-15)");
+		if (rn > 15 || rm > 15) throw new ArgumentException (HighRegisterIndexOutOfRange);
 		var n = (rn >> 3) & 1;
 		return (ushort)(0x4500 | (n << 7) | ((rm & 0xF) << 3) | (rn & 0x7));
 	}
 
-	public static uint Dmb ()
-	{
-		return 0x8f4ff3bfu;
-	}
+	public const uint Dmb = 0x8f4ff3bfu;
 
-	public static uint Dsb ()
-	{
-		return 0x8f4ff3bf;
-	}
+	public const uint Dsb = 0x8f4ff3bf;
 
 	public static ushort Eors (uint rdn, uint rm)
 	{
-		if (rdn > 7 || rm > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rdn > 7 || rm > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		return (ushort)(0x4040 | ((rm & 7) << 3) | rdn & 0x7);
 	}
 
-	public static uint Isb ()
-	{
-		return 0x8f6ff3bf;
-	}
+	public const uint Isb = 0x8f6ff3bf;
 
 	public static ushort Mov (uint rd, uint rm)
 	{
-		if (rd > 15 || rm > 15) throw new ArgumentException ("Register index out of range (0-15)");
+		if (rd > 15 || rm > 15) throw new ArgumentException (HighRegisterIndexOutOfRange);
 		return (ushort)(0x4600 | ((rd & 8) << 4) | ((rm & 0xF) << 3) | (rd & 7));
 	}
 
 	public static ushort Movs (uint rd, uint imm8)
 	{
-		if (rd > 15) throw new ArgumentException ("Register index out of range (0-15)");
+		if (rd > 15) throw new ArgumentException (HighRegisterIndexOutOfRange);
 		if (imm8 > 255) throw new ArgumentException ("Immediate too large for MOVS");
 		return (ushort)(0x2000 | (rd & 7) << 8 | imm8 & 0xFF);
 	}
 
 	public static ushort Muls (uint rn, uint rdm)
 	{
-		if (rn > 7 || rdm > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rn > 7 || rdm > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		return (ushort)(0x4340 | ((rn & 7) << 3) | (rdm & 7));
 	}
 
 	public static ushort Mvns (uint rd, uint rm)
 	{
-		if (rd > 15 || rm > 15) throw new ArgumentException ("Register index out of range (0-15)");
+		if (rd > 15 || rm > 15) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		return (ushort)(0x43c0 | ((rm & 7) << 3) | (rd & 7));
 	}
 
-	public static ushort Nop ()
-	{
-		return 0xBF00;
-	}
+	public const ushort Nop = 0xBF00;
 
 	public static ushort Orrs (uint rn, uint rm)
 	{
-		if (rn > 7 || rm > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rn > 7 || rm > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		return (ushort)(0x4300 | ((rm & 7) << 3) | rn & 7);
 	}
 
@@ -225,14 +216,14 @@ public static class InstructionEmiter
 
 	public static uint Mrs (uint rd, uint specialRegister)
 	{
-		if (rd > 15) throw new ArgumentException ("Register index out of range (0-15)");
+		if (rd > 15) throw new ArgumentException (HighRegisterIndexOutOfRange);
 		if (specialRegister > 255) throw new ArgumentException ("Special register index out of range (0-15)");
 		return 0x80000000 | (rd & 0xf) << 24 | (specialRegister & 0xFF) << 16 | 0xf3ef;
 	}
 
 	public static uint Msr (uint specialRegister, uint rd)
 	{
-		if (rd > 15) throw new ArgumentException ("Register index out of range (0-15)");
+		if (rd > 15) throw new ArgumentException (HighRegisterIndexOutOfRange);
 		if (specialRegister > 255) throw new ArgumentException ("Special register index out of range (0-15)");
 		return 0x88000000 | (specialRegister & 0xFF) << 16 | 0xf380 | rd & 0xf;
 	}
@@ -240,20 +231,20 @@ public static class InstructionEmiter
 	public static ushort Ldmia (uint rn, uint registerList)
 	{
 		if (registerList > 255) throw new ArgumentException ("Register list too large (0-15)");
-		if (rn > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rn > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		return (ushort)(0xC800 | (rn & 0x7) << 8 | registerList & 0xFF);
 	}
 
 	public static ushort LslsImm5 (uint rd, uint rm, uint imm5)
 	{
-		if (rd > 7 || rm > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rd > 7 || rm > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		if (imm5 > 31) throw new ArgumentException ("Immediate too large for LSLS");
-		return (ushort)(0x0000 | (imm5 & 0x1F) << 6 | ((rm & 7) << 3) | (rd & 7));
+		return (ushort)((imm5 & 0x1F) << 6 | ((rm & 7) << 3) | (rd & 7));
 	}
 
 	public static ushort LslsRegister (uint rdn, uint rm)
 	{
-		if (rdn > 7 || rm > 7) throw new ArgumentException ("Register index out of range (0-7)");
+		if (rdn > 7 || rm > 7) throw new ArgumentException (LowRegisterIndexOutOfRange);
 		return (ushort)(0x4080 | ((rm & 7) << 3) | rdn);
 	}
 }
